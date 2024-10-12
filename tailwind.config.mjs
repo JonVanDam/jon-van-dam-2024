1d0dd104fd6ac28ac3ce47ae2a981c5d561ddc1b
Jef Ceuppens <jefceuppens@MacBook-Pro-van-Jef.local>
2024-10-12 17:54:23 +0200

    implemented dark mode and button styling setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: '#37786c',
+        'theme-hover': '#37786ccc',
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    function ({ addComponents }) {
+      addComponents({
+        //borders
+        '.border-primary': {
+          '@apply border-zinc-300 dark:border-zinc-700': {},
+        },
+        '.border-secondary': {
+          '@apply border-zinc-500 dark:border-zinc-500': {},
+        },
+        //backgrounds
+        '.bg-primary': {
+          '@apply bg-zinc-50 dark:bg-zinc-900': {},
+        },
+        '.bg-secondary': {
+          '@apply bg-zinc-200 dark:bg-zinc-700': {},
+        },
+        '.bg-tertiary': {
+          '@apply bg-zinc-300 dark:bg-zinc-600': {},
+        },
+        //typography
+        '.text-primary': {
+          '@apply text-zinc-900 dark:text-zinc-100': {},
+        },
+        '.text-secondary': {
+          '@apply text-zinc-800 dark:text-zinc-200': {},
+        },
+        '.text-tertiary': {
+          '@apply text-zinc-600 dark:text-zinc-400': {},
+        },
+      });
+    },
+  ],
 };
